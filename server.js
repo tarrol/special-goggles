f5d4598232ec572c4a7720b0bbe0e477193d5615
Tarrol <quickandrey@gmail.com>
2022-12-14 19:10:43 -0800

    It works! Added a route to send the requester an 'index.html' repsonse.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const htmlRoute = require('./routes/htmlRoute')
 const app = express();
 const PORT = process.env.PORT || 3001
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/', htmlRoute)
+
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
