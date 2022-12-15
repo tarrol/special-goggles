ed4d5879aa6e10454daf67eb03aa9bd325a5ecfa
Tarrol <quickandrey@gmail.com>
2022-12-14 19:05:40 -0800

    Created a server.js and made it so it listens on port 3001. Currently returns a 'could not GET' error which is fine because it worked

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const apiRoute = require('./routes/apiRoute')
+const htmlRoute = require('./routes/htmlRoute')
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
