6c8faf8054aa261b8626430e998e0f2d88921535
Tarrol <quickandrey@gmail.com>
2023-01-09 20:47:04 -0800

    Missing a referenced 'store.js'. It's fixed, but doesn't do anything at the moment.

---
+++ db/store.js
@@ -0,0 +1,18 @@
+const util = require('util');
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+}
+
+module.exports = new Store();
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,5 @@
-const store = require('../../db/store');
 const router = require('express').Router();
+const store = require('../db/store');
 
 router.get('/notes', (req, res) => {
   store
@@ -16,3 +16,5 @@ router.post('/notes', (req, res) => {
     .then((note) => res.json(note))
     .catch((err) => res.status(500).jason(err));
 })
+
+module.exports = router;
