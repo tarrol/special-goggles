bdcf90631604b32afa89f9212e8edc1f40a5c090
Tarrol <quickandrey@gmail.com>
2023-01-09 20:38:18 -0800

    Typo in the first router.get, and missing parenthesis

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -5,9 +5,9 @@ router.get('/notes', (req, res) => {
   store
     .getNotes()
     .then((notes) => {
-      return.res.json(notes);
+      return res.json(notes);
     })
-    .catch((err) => res.status(500.json(err));
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
