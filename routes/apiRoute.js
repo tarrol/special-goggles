b8c0b8c35553eb65b4e9ba1e1ea6b8c20f53df24
Tarrol <quickandrey@gmail.com>
2023-01-09 20:36:25 -0800

    Added routes for home page to navigate to the notes page but it isn't working

---
+++ routes/apiRoute.js
@@ -0,0 +1,18 @@
+const store = require('../../db/store');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => {
+      return.res.json(notes);
+    })
+    .catch((err) => res.status(500.json(err));
+});
+
+router.post('/notes', (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).jason(err));
+})
--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,4 +5,8 @@ router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
 module.exports = router
