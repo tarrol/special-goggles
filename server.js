f259611efe3b4b8a9f3fb853bc607187cc4970ca
Tarrol <quickandrey@gmail.com>
2023-01-14 12:07:47 -0800

    Fixed broken layout on /notes

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const htmlRoute = require('./routes/htmlRoute')
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/', htmlRoute)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
 
 
 
